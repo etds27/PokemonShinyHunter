ee60ffe15e512f403e3546cc921b834345f191e7
Ethan Schwarz <etds27@gmail.com>
2023-10-26 20:09:27 -0700

    Added models for positioning

---
+++ src/Positioning/Models/GSCPositioning.lua
@@ -0,0 +1,28 @@
+local Model = {
+    Direction = {
+        addr = 0xD4DE,
+        size = 1,
+        NORTH = 4,
+        EAST = 12,
+        SOUTH = 0,
+        WEST = 8,
+    }
+
+    PositionX = {
+        addr = 0xDCB8,
+        size = 1,
+    }
+
+    PositionY = {
+        addr = 0xDCB7,
+        size = 1,
+    }
+
+    Map = {
+        addr = 0xDC86,
+        size = 1,
+        NewBarkTown = 6,
+        Route29 = 62,
+    }
+}
+return Model
