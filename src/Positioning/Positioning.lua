db4d1d85a25345a60efa80610022ddb75b4313c5
Ethan Schwarz <etds27@gmail.com>
2023-10-26 20:09:01 -0700

    Added models for positioning

--- src/Positioning/Positioning.lua
+++ src/Positioning/Positioning.lua
@@ -5,31 +5,16 @@ require "Input"
 
 Positioning = {}
 
-Direction = {
-    addr = 0xD4DE,
-    size = 1,
-    NORTH = 4,
-    EAST = 12,
-    SOUTH = 0,
-    WEST = 8,
-}
+-- Abstract tables
+local Model = {}
+Model.Direction = {}
+Model.PositionX = {}
+Model.PositionY = {}
+Model.Map = {}
+local Model = PositioningFactory:loadModel()
 
-PositionX = {
-    addr = 0xDCB8,
-    size = 1,
-}
-
-PositionY = {
-    addr = 0xDCB7,
-    size = 1,
-}
-
-Map = {
-    addr = 0xDC86,
-    size = 1,
-    NewBarkTown = 6,
-    Route29 = 62,
-}
+-- Merge model into class
+Positioning = Common:tableMerge(Positioning, Model)
 
 function Positioning:waitForOverworld(frameLimit)
     --[[
@@ -44,4 +29,19 @@ end
 
 function Positioning:inOverworld() 
     return Memory:readFromTable(Battle.PokemonTurnCounter) ~= 0
+end
+
+function Positioning:getPosition()
+    --[[
+        Get all position data for the player
+
+        Returns: Table with position data
+            {x: y: direction: map:}
+    ]]
+    t = {}
+    t.x = Memory:readFromTable(Positioning.PositionX)
+    t.y = Memory:readFromTable(Positioning.PositionY)
+    t.direction = Memory:readFromTable(Positioning.Direction)
+    t.map = Memorty:readFromTable(Positioning.Map)
+    return t
 end
