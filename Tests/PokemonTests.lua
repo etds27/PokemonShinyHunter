930422d09d7b381d3c6367b5208ca9586084bdce
Ethan Schwarz <etds27@gmail.com>
2023-10-28 16:10:10 -0700

    Added pokemon tests

---
+++ Tests/PokemonTests.lua
@@ -0,0 +1,23 @@
+PokemonTest = {}
+
+function Pokemon:testPartyPokemon()
+    savestate.load(TestStates.POKEMON_TESTS)
+    pokemon = Party:getPokemonAtIndex(1)
+    return pokemon.species == 155 and pokemon.movePp3 == 20 and pokemon.speedStat == 18
+end
+
+function Pokemon:testEnemyPokemon()
+    savestate.load(TestStates.POKEMON_TESTS)
+    pokemon = Pokemon:new(Pokemon.PokemonType.WILD, GameSettings.wildpokemon, Memory.WRAM)
+    return pokemon.species == 167 and pokemon.level == 4
+end
+
+function Pokemon:testBoxPokemon()
+    savestate.load(TestStates.POKEMON_TESTS)
+    pokemon = Box:getBox(1)[1]
+    return pokemon.species == 167 and pokemon.trainerId == 51032
+end
+
+print("Pokemon:testPartyPokemon()", Pokemon:testPartyPokemon())
+print("Pokemon:testEnemyPokemon()", Pokemon:testEnemyPokemon())
+print("Pokemon:testBoxPokemon()", Pokemon:testBoxPokemon())
