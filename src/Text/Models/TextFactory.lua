408c441e28c99b77305fe87fdfb06426d5119575
Ethan Schwarz <etds27@gmail.com>
2023-10-28 15:23:30 -0700

    Fixed Text factory

--- src/Text/Models/TextFactory.lua
+++ src/Text/Models/TextFactory.lua
@@ -1,7 +1,9 @@
 TextFactory = {}
 
 function TextFactory:loadModel()
+    Common:resetRequires({"GSCText"})
     if Common:contains(GameGroups.GEN_2, GameSettings.game) then
         return require("GSCText")
     end
+    return {}
 end
