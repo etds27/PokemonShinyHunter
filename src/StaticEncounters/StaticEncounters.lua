4185f8641fdc2cd4dba52ee296448cdb90c5ca91
Ethan Schwarz <etds27@gmail.com>
2023-11-07 18:43:19 -0800

    moved static encounters

--- src/StaticEncounters/StaticEncounters.lua
+++ src/StaticEncounters/StaticEncounters.lua
@@ -9,5 +9,12 @@ StaticEncounters = {}
 -- Abstract tables
 local Model = StaticEncountersFactory:loadModel()
 
+StaticEncounters.standardWildEncounter = function()
+    -- Start Dialog
+    Input:pressButtons{buttonKeys={Buttons.A}, duration=14, waitFrames=2}
+    return Battle:waitForBattleMenu()
+end
+
+
 StaticEncounters = Common:tableMerge(StaticEncounters, Model)
 
