0812b0bce1334c6c7a0a7446f2559894d8529134
Ethan Schwarz <etds27@gmail.com>
2023-11-14 19:04:22 -0800

    Update GS menu model

---
+++ src/Menu/Models/CrystalMenu.lua
@@ -0,0 +1,18 @@
+local Model = {
+    Cursor = {
+        addr = 0xCFA9,
+        size = 1
+    },
+
+    MultiCursor = {
+        X = {
+            addr = 0xCFA9,
+            size = 1
+        },
+        Y = {
+            addr = 0xCFA8,
+            size = 1
+        }
+    }
+}
+return Model
---
+++ src/Menu/Models/GSMenu.lua
@@ -0,0 +1,18 @@
+local Model = {
+    Cursor = {
+        addr = 0xCEE0,
+        size = 1
+    },
+
+    MultiCursor = {
+        X = {
+            addr = 0xCEE1,
+            size = 1
+        },
+        Y = {
+            addr = 0xCEE0,
+            size = 1
+        }
+    }
+}
+return Model
