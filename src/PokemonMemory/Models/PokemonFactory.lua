6ed5af50920e60ef256755de5d88898504b13b4b
Ethan Schwarz <etds27@gmail.com>
2023-11-16 18:43:32 -0800

    Added GS Pokemon model

--- src/PokemonMemory/Models/PokemonFactory.lua
+++
@@ -1,8 +0,0 @@
-PokemonFactory = {}
-
-function PokemonFactory:loadModel()
-    Common:resetRequires({"GSCPokemon"})
-    if Common:contains(GameGroups.GEN_2, GameSettings.game) then
-        return require("GSCPokemon")
-    end
-end
