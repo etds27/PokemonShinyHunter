f7d3183f91336fef6019a9a18d624981e7df17ab
Ethan Schwarz <etds27@gmail.com>
2024-01-24 20:08:34 -0800

    Added discord webhook

--- src/Python/Server/event_handler.py
+++ src/Python/Server/event_handler.py
@@ -1,6 +1,5 @@
+import ../discord_client
 import enum
-import logging
-import time
 import rom_info
 
 from collection_manager import CollectionManager
@@ -13,6 +12,9 @@ class Event(enum.Enum):
     GAME = "game"
 
 class EventHandler:
+    """
+    Class to handle the events recieved from the emulator and provide the content to the appropriate event manager
+    """
     def __init__(self) -> None:
         self.bot_agents = {}
         
@@ -38,4 +40,48 @@ class EventHandler:
             Event.COLLECTION.value: CollectionManager(bot_id=bot_id),
             Event.GAME.value: GameStatsManager(bot_id=bot_id)
         }
+
+# Callbacks for specific event managers to make when an event happens
+def on_new_shiny(bot_id, shiny_payload):
+    """
+    Global actions to take when there is a new shiny found
+
+    shiny_payload:
+    {
+        "timestamp": int,
+        "pokemon": {
+            "species": str,
+            "name": str,
+            "variant": str
+        },
+        "phase_length": int,
+        "total_encounters": int,
+        "total_species_seen": int,
+
+        "total_shinies_found": int,
+        "total_shiny_species_seen": int,
+        "shinies_needed": int
+    }
+    """
+    message = discord_client.create_shiny_message(bot_id=bot_id, shiny_payload=shiny_payload)
+    discord_client.send_discord_message(bot_id, message=message)
+
+if __name__ == "__main__":
+    bot_id = "ETHAN12345"
+    shiny_payload = {
+        "timestamp": 1000,
+        "pokemon": {
+            "species": "TEST",
+            "name": "Victrebel",
+            "variant": "TEST"
+        },
+        "phase_length": 20034,
+        "total_encounters": 1000000,
+        "total_species_seen": 40000,
+
+        "total_shinies_found": 942,
+        "total_shiny_species_seen": 40,
+        "shinies_needed": 3        
+    }
+    on_new_shiny(bot_id, shiny_payload)
 
---
+++ src/Python/discord_client.py
@@ -0,0 +1,53 @@
+import os
+import pprint
+import requests
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL", "")
+
+def create_shiny_message(bot_id, shiny_payload):
+    s = ""
+    s += f"# {bot_id} just caught a shiny {shiny_payload['pokemon']['name']}\n\n"
+    s += "```"
+    for key, value in [(f"Time", shiny_payload["timestamp"]),
+                       (f"Name", shiny_payload["pokemon"]["name"]),
+                       (f"Phase Length", shiny_payload["phase_length"]),
+                       (f"Pokemon Seen", shiny_payload["total_encounters"]),
+                       (f"Shinies Seen", shiny_payload["total_shinies_found"]),
+                       (f"{shiny_payload['pokemon']['name']} Seen", shiny_payload["total_species_seen"]),
+                       (f"Shiny {shiny_payload['pokemon']['name']} Seen", shiny_payload["total_shiny_species_seen"]),
+                       (f"{shiny_payload['pokemon']['name']} Needed", shiny_payload["shinies_needed"]),
+                       ]:
+        pass
+        s += f"{key + ':':<23}{value}\n"
+    s += "```"
+    
+    return s
+
+def send_discord_message(message: str):
+    if not WEBHOOK_URL:
+        return
+    payload = {"content": message} 
+    print(requests.post(WEBHOOK_URL, json=payload))
+
+if __name__ == "__main__":
+    bot_id = "ETHAN12345"
+    shiny_payload = {
+        "timestamp": 1000,
+        "pokemon": {
+            "species": "TEST",
+            "name": "Victreebel",
+            "variant": "TEST"
+        },
+        "phase_length": 20034,
+        "total_encounters": 1000000,
+        "total_species_seen": 40000,
+
+        "total_shinies_found": 942,
+        "total_shiny_species_seen": 40,
+        "shinies_needed": 3        
+    }
+    message = create_shiny_message(bot_id, shiny_payload)
+    send_discord_message(message=message)
